42eabbad1717586357df05ec6a99fb5addd2a4f1
thumb0422 <thumb0422@163.com>
2019-06-09 13:50:58 +0800

    add file

---
+++ HimsSoft.dpr
@@ -0,0 +1,14 @@
+program HimsSoft;
+
+uses
+  Vcl.Forms,
+  MainPage in 'MainPage.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
