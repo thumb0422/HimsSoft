cb70f68c9d51712589c30a23ca6d5e8eda609ad3
thumb0422 <thumb0422@163.com>
2019-06-11 21:03:12 +0800

    update

--- MainPage.pas
+++ MainPage.pas
@@ -33,14 +33,14 @@ procedure TForm1.initBtnClick(Sender: TObject);
 var
   ss: string;
 begin
-  FCom32.init('');
+  FCom32.init;
 end;
 
 procedure TForm1.sendBtnClick(Sender: TObject);
 var
   ss: string;
 begin
-  FCom32.send('');
+  FCom32.send;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -52,7 +52,7 @@ begin
   comList.Add('3');
   comList.Add('4');
   FCom32 := THComm.Create(comList);
-  FCom32.interval = 1000;
+  FCom32.interval := 1000;
   FCom32.sendData := '0A 0D 4B';
 end;
 
