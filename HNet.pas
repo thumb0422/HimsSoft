b0d0b5282ddb5aa29aa72a055665f10ce629beb2
thumb0422 <thumb0422@163.com>
2019-06-13 21:38:58 +0800

    update net

--- HNet.pas
+++ HNet.pas
@@ -3,11 +3,9 @@ unit HNet;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes,
-  System.Generics.Collections, Vcl.Graphics, Vcl.Controls, Vcl.Forms,
-  Vcl.Dialogs,
-  Vcl.ComCtrls, Vcl.StdCtrls, Vcl.ExtCtrls, System.Win.ScktComp, HLog,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  System.Generics.Collections, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  Vcl.ComCtrls, Vcl.StdCtrls, Vcl.ExtCtrls, System.Win.ScktComp,System.Typinfo, HLog,
   HDeviceInfo;
 
 type
@@ -29,8 +27,7 @@ type
 
     procedure ClientSocketConnect(Sender: TObject; Socket: TCustomWinSocket);
     procedure ClientSocketDisconnect(Sender: TObject; Socket: TCustomWinSocket);
-    procedure ClientSocketError(Sender: TObject; Socket: TCustomWinSocket;
-      ErrorEvent: TErrorEvent; var ErrorCode: Integer);
+    procedure ClientSocketError(Sender: TObject; Socket: TCustomWinSocket; ErrorEvent: TErrorEvent; var ErrorCode: Integer);
     procedure ClientSocketRead(Sender: TObject; Socket: TCustomWinSocket);
     procedure ClientSocketWrite(Sender: TObject; Socket: TCustomWinSocket);
     procedure onNetWriteData(Sender: TObject);
@@ -66,21 +63,10 @@ begin
       netIPObj.OnError := ClientSocketError;
       netIPObj.OnRead := ClientSocketRead;
       netIPObj.OnWrite := ClientSocketWrite;
-      try
-        netIPObjDic.AddOrSetValue(fdeviceTag, netIPObj);
-        TLog.Instance.DDLogInfo('NET ' + fdeviceInfo.dName + ':' +
-          IntToStr(fdeviceInfo.dPort) + ' open');
-        netIPObj.Active := False;
-        netIPObj.Active := True;
-      except
-        on E: Exception do
-        begin
-          netIPObjDic.Remove(fdeviceTag);
-          TLog.Instance.DDLogError('NET ' + fdeviceInfo.dName + ':' +
-            IntToStr(fdeviceInfo.dPort) + ' openError');
-          netIPObj.Active := False;
-        end;
-      end;
+//      netIPObjDic.AddOrSetValue(fdeviceTag, netIPObj);
+      TLog.Instance.DDLogInfo('NET ' + fdeviceInfo.dName + ':' + IntToStr(fdeviceInfo.dPort) + ' connecting');
+      netIPObj.Active := False;
+      netIPObj.Active := True;
     end;
 
   end;
@@ -107,21 +93,10 @@ begin
       netIPObj.OnError := ClientSocketError;
       netIPObj.OnRead := ClientSocketRead;
       netIPObj.OnWrite := ClientSocketWrite;
-      try
-        netIPObjDic.AddOrSetValue(fdeviceTag, netIPObj);
-        TLog.Instance.DDLogInfo('NET ' + deviceInfo.dName + ':' +
-          IntToStr(deviceInfo.dPort) + ' open');
-        netIPObj.Active := False;
-        netIPObj.Active := True;
-      except
-        on E: Exception do
-        begin
-          netIPObj.Active := False;
-          netIPObjDic.Remove(fdeviceTag);
-          TLog.Instance.DDLogError('NET ' + deviceInfo.dName + ':' +
-            IntToStr(deviceInfo.dPort) + ' openError');
-        end;
-      end;
+//      netIPObjDic.AddOrSetValue(fdeviceTag, netIPObj);
+      TLog.Instance.DDLogInfo('NET ' + deviceInfo.dName + ':' + IntToStr(deviceInfo.dPort) + ' connecting');
+      netIPObj.Active := False;
+      netIPObj.Active := True;
     end;
   end;
 end;
@@ -148,8 +123,7 @@ begin
     if Assigned(netIPObj) then
     begin
       netIPObjDic.Remove(IntToStr(netIPObj.tag));
-      TLog.Instance.DDLogInfo('NET ' + netIPObj.Address + ':' +
-        IntToStr(netIPObj.Port) + ' stopNet');
+      TLog.Instance.DDLogInfo('NET ' + netIPObj.Address + ':' + IntToStr(netIPObj.Port) + ' stopNet');
       netIPObj.Active := False;
     end;
   end;
@@ -193,8 +167,7 @@ begin
       if Assigned(deviceInfo) then
       begin
         commonData := deviceInfo.dCommond;
-        TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' +
-          IntToStr(netIPObj.Port) + ' writeData: ' + commonData);
+        TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' + IntToStr(netIPObj.Port) + ' writeData: ' + commonData);
         netIPObj.Socket.SendText(commonData);
       end;
     end;
@@ -207,36 +180,45 @@ var
   netIPObj: TClientSocket;
 begin
   netIPObj := TClientSocket(Sender);
-  TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' +
-    IntToStr(netIPObj.Port) + ' connect');
+  deviceInfo := getDeviceInfoFromNetObj(netIPObj);
+  if Assigned(deviceInfo) then
+  begin
+    netIPObjDic.AddOrSetValue(IntToStr(deviceInfo.dTag),netIPObj);
+    TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' + IntToStr(netIPObj.Port) + ' connected');
+  end;
+
 end;
 
-procedure TNet.ClientSocketDisconnect(Sender: TObject;
-  Socket: TCustomWinSocket);
+procedure TNet.ClientSocketDisconnect(Sender: TObject; Socket: TCustomWinSocket);
 var
-  keyTag: string;
-  commonData: string;
   deviceInfo: TDeviceInfo;
   netIPObj: TClientSocket;
 begin
   netIPObj := TClientSocket(Sender);
-  TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' +
-    IntToStr(netIPObj.Port) + ' Disconnect');
+  deviceInfo := getDeviceInfoFromNetObj(netIPObj);
+  if Assigned(deviceInfo) then
+  begin
+    netIPObjDic.Remove(IntToStr(deviceInfo.dTag));
+    TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' + IntToStr(netIPObj.Port) + ' Disconnect');
+  end;
 end;
 
-procedure TNet.ClientSocketError(Sender: TObject; Socket: TCustomWinSocket;
-  ErrorEvent: TErrorEvent; var ErrorCode: Integer);
+procedure TNet.ClientSocketError(Sender: TObject; Socket: TCustomWinSocket; ErrorEvent: TErrorEvent; var ErrorCode: Integer);
 var
-  keyTag: string;
-  commonData: string;
   deviceInfo: TDeviceInfo;
   netIPObj: TClientSocket;
+  originErrorCode :Integer;
 begin
+  originErrorCode :=ErrorCode;
   ErrorCode := 0;
   netIPObj := TClientSocket(Sender);
-  TLog.Instance.DDLogError('Net ' + netIPObj.Address + ':' +
-    IntToStr(netIPObj.Port) + ' errorCode = ' + IntToStr(ErrorCode));
-
+  deviceInfo := getDeviceInfoFromNetObj(netIPObj);
+  if Assigned(deviceInfo) then
+  begin
+    netIPObjDic.Remove(IntToStr(deviceInfo.dTag));
+    TLog.Instance.DDLogError('Net ' + netIPObj.Address + ':' + IntToStr(netIPObj.Port) +
+    ',ErrorEvent =' + GetEnumName(TypeInfo(TErrorEvent), ord(ErrorEvent)) +',errorCode = ' + IntToStr(originErrorCode));
+  end;
 end;
 
 procedure TNet.ClientSocketRead(Sender: TObject; Socket: TCustomWinSocket);
@@ -247,8 +229,8 @@ var
   netIPObj: TClientSocket;
 begin
   netIPObj := TClientSocket(Sender);
-  TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' +
-    IntToStr(netIPObj.Port) + ' read');
+//  netIPObj.Socket.
+  TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' + IntToStr(netIPObj.Port) + ' read');
 end;
 
 procedure TNet.ClientSocketWrite(Sender: TObject; Socket: TCustomWinSocket);
@@ -259,8 +241,7 @@ var
   netIPObj: TClientSocket;
 begin
   netIPObj := TClientSocket(Sender);
-  TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' +
-    IntToStr(netIPObj.Port) + ' write');
+  TLog.Instance.DDLogInfo('Net ' + netIPObj.Address + ':' + IntToStr(netIPObj.Port) + ' write');
 end;
 
 function TNet.getDeviceInfoFromNetObj(obj: TClientSocket): TDeviceInfo;
@@ -271,8 +252,7 @@ begin
   for i := 0 to netIPList.Count - 1 do
   begin
     device := netIPList.Items[i];
-    if (device.dName = obj.Address) and (device.dPort = obj.Port) and
-      (device.dTag = obj.tag) then
+    if (device.dName = obj.Address) and (device.dPort = obj.Port) and (device.dTag = obj.tag) then
     begin
       Result := device;
       Break;
@@ -292,3 +272,4 @@ begin
 end;
 
 end.
+
