0b4dfb6e8915251f912ad0b5e3719daa6026259b
thumb0422 <thumb0422@163.com>
2020-08-25 15:09:15 +0800

    update
    修改框架结构

--- HimsSoft.dpr
+++ HimsSoft.dpr
@@ -2,20 +2,47 @@ program HimsSoft;
 
 uses
   Vcl.Forms,
-  HCustomer in 'HCustomer.pas',
   MainPage in 'MainPage.pas' {FMainPage},
-  HRoom1 in 'HRoom1.pas' {RoomPage},
-  HBedView in 'HBedView.pas',
-  HMenu in 'HMenu.pas',
-  HDataDetailView in 'HDataDetailView.pas',
-  HDataNotify in 'HDataNotify.pas',
-  HSettingPage in 'HSettingPage.pas' {SettingPage},
-  HBedSetPage in 'HBedSetPage.pas' {BedSetPage},
-  HMechineSetPage in 'HMechineSetPage.pas' {MechineSetPage},
-  HCustomerSetPage in 'HCustomerSetPage.pas' {CustomerSetPage},
-  HCBMLinkPage in 'HCBMLinkPage.pas' {HCBMLinkPage},
-  HCBMLinkAddPage in 'HCBMLinkAddPage.pas' {CBMLinkAddPage},
-  HSecuritySetPage in 'HSecuritySetPage.pas' {SecuritySetPage};
+  HBasePage in 'source\BaseForm\HBasePage.pas',
+  HBedSetPage in 'source\Biz\HBedSetPage.pas' {BedSetPage},
+  HBedView in 'source\Biz\HBedView.pas',
+  HCBMLinkAddPage in 'source\Biz\HCBMLinkAddPage.pas' {CBMLinkAddPage},
+  HCBMLinkPage in 'source\Biz\HCBMLinkPage.pas' {HCBMLinkPage},
+  HCustomer in 'source\Biz\HCustomer.pas',
+  HCustomerSetPage in 'source\Biz\HCustomerSetPage.pas' {CustomerSetPage},
+  HDataDetailView in 'source\Biz\HDataDetailView.pas',
+  HMechineSetPage in 'source\Biz\HMechineSetPage.pas' {MechineSetPage},
+  HMenu in 'source\Biz\HMenu.pas',
+  HRoom1 in 'source\Biz\HRoom1.pas' {RoomPage},
+  HSecuritySetPage in 'source\Biz\HSecuritySetPage.pas' {SecuritySetPage},
+  HSettingPage in 'source\Biz\HSettingPage.pas' {SettingPage},
+  HCate in 'source\DeviceComm\Communicate\HCate.pas',
+  HCom32 in 'source\DeviceComm\Communicate\HCom32.pas',
+  HDeviceDo in 'source\DeviceComm\Communicate\HDeviceDo.pas',
+  HNet in 'source\DeviceComm\Communicate\HNet.pas',
+  HDeviceDefine in 'source\DeviceComm\Const\HDeviceDefine.pas',
+  HDeviceTool in 'source\DeviceComm\Const\HDeviceTool.pas',
+  HBellco in 'source\DeviceComm\DevicePrase\HBellco.pas',
+  HBraun in 'source\DeviceComm\DevicePrase\HBraun.pas',
+  HDeviceInfo in 'source\DeviceComm\DevicePrase\HDeviceInfo.pas',
+  HFresenius in 'source\DeviceComm\DevicePrase\HFresenius.pas',
+  HGambro in 'source\DeviceComm\DevicePrase\HGambro.pas',
+  HJason in 'source\DeviceComm\DevicePrase\HJason.pas',
+  HNikkiso in 'source\DeviceComm\DevicePrase\HNikkiso.pas',
+  HToray in 'source\DeviceComm\DevicePrase\HToray.pas',
+  HHistoryDataPage in 'source\Report\HHistoryDataPage.pas' {HistoryDataPage},
+  SQLite3 in 'source\Utility\plugin\SQLite3.pas',
+  SQLiteTable3 in 'source\Utility\plugin\SQLiteTable3.pas',
+  superdate in 'source\Utility\plugin\superdate.pas',
+  superobject in 'source\Utility\plugin\superobject.pas',
+  supertimezone in 'source\Utility\plugin\supertimezone.pas',
+  supertypes in 'source\Utility\plugin\supertypes.pas',
+  superxmlparser in 'source\Utility\plugin\superxmlparser.pas',
+  HDBManager in 'source\Utility\HDBManager.pas',
+  HLog in 'source\Utility\HLog.pas',
+  HTool in 'source\Utility\HTool.pas',
+  HDataModel in 'source\DeviceComm\DeviceInfo\HDataModel.pas',
+  HDataNotify in 'source\DeviceComm\DeviceInfo\HDataNotify.pas';
 
 {$R *.res}
 
@@ -23,5 +50,13 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFMainPage, FMainPage);
+  Application.CreateForm(TBedSetPage, BedSetPage);
+  Application.CreateForm(TCBMLinkAddPage, CBMLinkAddPage);
+  Application.CreateForm(TCustomerSetPage, CustomerSetPage);
+  Application.CreateForm(TMechineSetPage, MechineSetPage);
+  Application.CreateForm(TRoomPage, RoomPage);
+  Application.CreateForm(TSecuritySetPage, SecuritySetPage);
+  Application.CreateForm(TSettingPage, SettingPage);
+  Application.CreateForm(THistoryDataPage, HistoryDataPage);
   Application.Run;
 end.
